d73b054a5a4b981f5fcb676c4377dc84cf2f6a96
Eduardo Ceron <ing.ceron@192.168.5.31>
2023-01-06 05:35:22 -0600

    Initial Commit

---
+++ Pinch/Model/PageModel.swift
@@ -0,0 +1,14 @@
+
+
+import Foundation 
+
+struct Page: Identifiable {
+  let id: Int
+  let imageName: String
+}
+
+extension Page {
+  var thumbnailName: String {
+    return "thumb-" + imageName
+  }
+}
